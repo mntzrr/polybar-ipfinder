d12e0a80d6ed4b659b76a133e5c7c8b611eabb04
Montazar Al-Jaber <26507935+muntjaber@users.noreply.github.com>
2021-02-23 22:07:16 +0100

    Add nordlynx to recognized VPN tunnels

--- ipfinder.sh
+++ ipfinder.sh
@@ -69,8 +69,7 @@ while :; do
 
     status=$VPN_DOWN
     # If a VPN connection is established, a tunnel is created.
-    if ip tuntap | grep -iEq 'tun[0-9]+'; then
-
+    if ip tuntap | grep -iEq '(tun[0-9]+|nordlynx)'; then
 	status=$VPN_UP
     fi
 
